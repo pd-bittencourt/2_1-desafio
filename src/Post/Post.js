fdc109db2fdf8e44a8e78ccca70753684337ff95
Pedro <pedrodmbc@gmail.com>
2019-05-16 11:58:00 -0300

    Add proptypes & fix lint problems
    Lint problem was destructuring the props

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import PostHeader from './PostHeader';
 
 import './styles.css';
 
-const Post = props => (
+const Post = ({ data }) => (
   <div className="post-wrapper">
-    <PostHeader avatar={props.data.avatar} name={props.data.name} time={props.data.time} />
-    <p className="post-body">{props.data.body}</p>
+    <PostHeader avatar={data.avatar} name={data.name} time={data.time} />
+    <p className="post-body">{data.body}</p>
   </div>
 );
+
+Post.defaultProps = {
+  data: null,
+};
+
+Post.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+  }),
+};
 
 export default Post;
--- src/Post/PostHeader.js
+++ src/Post/PostHeader.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PostHeader = props => (
+const PostHeader = ({ avatar, name, time }) => (
   <div className="post-header">
-    <img src={props.avatar} alt="" className="avatar" />
+    <img src={avatar} alt="" className="avatar" />
     <div className="post-header-info">
-      <h2>{props.name}</h2>
-      <p>{props.time}</p>
+      <h2>{name}</h2>
+      <p>{time}</p>
     </div>
   </div>
 );
+
+PostHeader.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+};
 
 export default PostHeader;
